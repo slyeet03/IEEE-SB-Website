63cbbcbf74f58add6b5bbb966d9539c6d4a19da5
tusharb05 <tusharbaweja2005@gmail.com>
2025-01-21 23:02:47 +0530

    setup

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-slate-300">
+      <h1 className="text-4xl font-bold text-blue-500">Testing...</h1>
+    </div>
+  );
+}
