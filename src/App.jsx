c573ad6816921360ee1c7712bcdb8bcee23175da
Harshit Singh Raushan <155360091+HarshitSinghRaushan@users.noreply.github.com>
2025-01-26 16:48:43 +0000

    fixed events route, added achievement route

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Events from "./components/Events";
 import Testimonials from "./components/Testimonials";
+import Achievements from "./components/Achievements";
 import BlogPosts from "./components/BlogPosts";
 import Footer from "./components/Footer";
 import Societies from "./components/Societies"; 
-import Contact from "./components/Contact"; 
+import Contact from "./components/Contact";
 
 const Home = () => (
   <>
@@ -16,6 +17,7 @@ const Home = () => (
     <Events />
     <Testimonials />
     <BlogPosts />
+    <Achievements />
   </>
 );
 
@@ -28,7 +30,11 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route
             path="/events"
-            element={<div className="pt-20">Events Page</div>}
+            element={
+              <div className="pt-20">
+                <Events />
+              </div>
+            }
           />
           <Route
             path="/blog"
@@ -39,6 +45,15 @@ function App() {
             element={
               <div className="pt-20">
                 <Societies />
+              </div>
+            }
+          
+          />
+          <Route
+            path="/achievements"
+            element={
+              <div className="pt-20">
+                <Achievements />
               </div>
             }
           />
