ee15c3d747142e78130cae59e4e7a7a951837b39
Yogesh Jajoria <yogeshjajoria2019@gmail.com>
2025-06-09 16:09:07 +0530

    removed horizontalScroll replaced with segmented control tabs

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import HorizontalScroll from "../components/HorizontalScroll";
 import SwipeCards from "../components/SwipeCards";
+import EventGrid from "../components/EventGrid";
 
 export default function Events() {
   return (
     <section className="w-full bg-white dark:bg-ieee-dark">
       <div className="w-full px-4 sm:px-6 lg:px-8">
         <SwipeCards />
-        <HorizontalScroll />
+        <EventGrid />
       </div>
     </section>
   );
